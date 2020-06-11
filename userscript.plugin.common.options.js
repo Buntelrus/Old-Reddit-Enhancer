86f110cdbd1249b9b7c94f93b7c84e57b0fb0c26
buntel <julianbantel@gmail.com>
2020-06-11 19:17:34 +0200

    fix update/download urls

--- userscript.plugin.common.options.js
+++ userscript.plugin.common.options.js
@@ -4,8 +4,8 @@ module.exports = {
     name: 'Old Reddit Enhancer',
     version: '[version]',
     match: 'https://old.reddit.com/r/*',
-    updateURL: 'https://gist.github.com/Buntelrus/6914a08285e215fdd9c2530661d9d81c/raw/736b0ede4d513b9115846b05899f7a10416f1789/main.bundle.min.user.js',
-    downloadURL: 'https://gist.github.com/Buntelrus/6914a08285e215fdd9c2530661d9d81c/raw/736b0ede4d513b9115846b05899f7a10416f1789/main.bundle.min.user.js',
+    updateURL: 'https://gist.github.com/Buntelrus/6914a08285e215fdd9c2530661d9d81c/raw/main.bundle.min.user.js',
+    downloadURL: 'https://gist.github.com/Buntelrus/6914a08285e215fdd9c2530661d9d81c/raw/main.bundle.min.user.js',
     description: 'hide comments.',
   },
 }
